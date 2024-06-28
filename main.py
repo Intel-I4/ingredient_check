1dcdc992ee25dbe13bafddfa280ef371fb286318
DH117117 <h3571132@gmail.com>
2024-06-28 11:35:09 +0900

    main re-modify

--- main.py
+++ main.py
@@ -1,67 +1,104 @@
+import os
+import threading
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 
-from ui import fridge_ui
+from sock import server
+from ui import fridge_ui, recipe_ui
 from cam.webcam import Webcam
-from database import recipe_ingredient_db
 
 ########## 전역 변수부 ##########
 page = 1
 webcam = None
 themeColor = "#f6ddd9"
 recipe = 0
+server_ip = "10.10.15.103"
+server_port = 12309
+frame2 = None
+recipe_frame = []
+
+
 
 ########## 함수 정의부 ##########
 
-def next_frame(btn = 0):
-    global page, webcam, recipe
+def next_frame(btn=0):
+    global page, webcam, recipe, frame2, recipe_frame
+
     page += 1
-    if page >= 4:
-        page = 4
-        fridge_ui.reclip_list(frame4, tk, recipe)   # recpie 값에 따라 해당 레시피 출력
-        frame4.lift()
-    elif page == 3:
-        recipe = btn              # 레시피 버튼 값 recipe 변수에 저장
-        
+    if page > 3 + len(recipe_frame):
+        print("exit")
+        os._exit(os.EX_OK)
+
+    elif page > 3 and page <= 3 + len(recipe_frame):
         if webcam is not None:
-            webcam.start_webcam(recipe)  # frame3이 보일 때 웹캠 시작
-            
+            webcam.stop_webcam()   # frame2에서는 웹캠 정지
+        recipe_frame[page-4].lift()
+
+    elif page == 3:
+        recipe = btn               # 레시피 버튼 값 recipe 변수에 저장
+        recipe_frame = recipe_ui.recipe_frame(root, win_height, win_width,
+                                              recipe, next_frame, prev_frame)
+        if webcam is not None:
+             webcam.start_webcam(recipe)  # frame3이 보일 때 웹캠 시작
         frame3.lift()
+
     elif page == 2:
         if webcam is not None:
             webcam.stop_webcam()  # frame2에서는 웹캠 정지
         frame2.lift()
+
     elif page == 1:
         frame1.lift()
 
 
 def prev_frame():
-    global page, webcam
+    global page, webcam, frame2
+
     page -= 1
     if page <= 1:
         page = 1
         frame1.lift()
     elif page == 2:
+        if webcam is not None:
+            webcam.stop_webcam()   # frame2에서는 웹캠 정지
         frame2.lift()
     elif page == 3:
         if webcam is not None:
             webcam.start_webcam(recipe)  # frame3이 보일 때 웹캠 시작
         frame3.lift()
-    elif page == 4:
-        frame4.lift()
+    elif page > 3:
+        recipe_frame[page-4].lift()
 
 
 def load_image():
     try:
         # 이미지 로드 및 리사이징
-        image = Image.open("ui/main_page.jpeg")
+        image = Image.open("ui/image/main_page.jpeg")
         image = image.resize((300, 300), Image.LANCZOS)
         img = ImageTk.PhotoImage(image)
         return img
     except Exception as e:
         print(f"Error loading image: {e}")
         return None
+
+
+def reload_frame2():
+    global frame2
+
+    frame2 = tk.Frame(root, bg=themeColor, border=2)
+    frame2.place(x=0, y=0, width=win_width, height=win_height)
+    # 냉장고 속의 재료 출력
+    fridge_ui.fridge_list(frame2)
+    # 추천 레시피 순서를 sugestion_lst에 삽입
+    fridge_ui.recipe_sort()
+    # 추천 레시피 순서로 버튼 순서 변경
+    fridge_ui.recipe_button(frame2, next_frame)
+
+    # 버튼 frame2
+    but_frame2 = tk.Frame(frame2, border=10, bg=themeColor)
+    but_frame2.place(x=450, y=850, width=150, height=60)
+
 
 
 ##########  main ##########
@@ -131,11 +168,11 @@ tk.Button(but_frame3, text='이전', command=prev_frame).pack(side=tk.RIGHT)
 frame2 = tk.Frame(root, bg=themeColor, border=2)
 frame2.place(x=0, y=0, width=win_width, height=win_height)
 # 냉장고 속의 재료 출력
-fridge_ui.fridge_list(frame2, tk, ttk)
+fridge_ui.fridge_list(frame2)
 # 추천 레시피 순서를 sugestion_lst에 삽입
 fridge_ui.recipe_sort()
 # 추천 레시피 순서로 버튼 순서 변경
-fridge_ui.recipe_button(frame2, tk, next_frame)
+fridge_ui.recipe_button(frame2, next_frame)
 
 # 버튼 frame2
 but_frame2 = tk.Frame(frame2, border=10, bg=themeColor)
@@ -146,8 +183,8 @@ but_frame2.place(x=450, y=850, width=150, height=60)
 
 frame1 = tk.Frame(root, bg=themeColor, border=2)
 frame1.place(x=0, y=0, width=win_width, height=win_height)
-tk.Label(frame1, text="요린이를 위한\n레시피 추천", bg=themeColor, 
-         font=("Arial", 40,"bold")).place(x=170, y=180)
+tk.Label(frame1, text="요린이를 위한\n레시피 추천", bg=themeColor,
+         font=("Arial", 40, "bold")).place(x=170, y=180)
 
 # 이미지 로드
 main_img = load_image()
@@ -161,6 +198,14 @@ tk.Button(but_frame1, text='시작하기', command=next_frame,
           border=10).pack(fill='x', expand=True)
 
 
+# 냉장고 정보를 라즈베리 파이로부터 받아오기 위한 소켓 서버 생성 (w.thread)
+try:
+    freg_thrd = threading.Thread(target=server.receive_file,
+                                 args=(server_ip, server_port, reload_frame2))
+    freg_thrd.start()
+    print("Thread started successfully.")
+except Exception as e:
+    print(f"Error starting thread: {e}")
+
+
 root.mainloop()
-
-
